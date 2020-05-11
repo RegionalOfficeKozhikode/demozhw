44c091682b3561dab6c4140adbcffa491cabc8a6
KonzernTechie <kv.nijesh@gmail.com>
2020-05-11 19:07:09 +0530

    sample

---
+++ demozhw/src/test/java/com/proximotech/zhw/DemozhwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proximotech.zhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemozhwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
